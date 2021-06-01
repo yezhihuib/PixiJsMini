fc648a9b584a0ae0db5df5db85e2afae7f42caa0
yzh <yezhihui_bruce@hotmail.com>
2021-06-02 00:35:19 +0800

    commit

---
+++ client/res/fruitPicMap.ts
@@ -0,0 +1,10 @@
+export const fruitPicMap = {
+  1: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/1.png",
+  2: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/2.png",
+  3: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/3.png",
+  4: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/4.png",
+  5: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/5.png",
+  6: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/6.png",
+  7: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/7.png",
+  8: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/8.png",
+  9: "https://bruce-public-bucket.oss-cn-hangzhou.aliyuncs.com/game/pic/suika/9.png",
